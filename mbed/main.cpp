3fceb75ec07ad41472c7f9089db8837e0f62f678
R-Imai <s1426015eq@s.chibakoudai.jp>
2015-12-06 18:03:15 +0900

    add turn()

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -4,8 +4,14 @@ Motion robot;
 
 int main()
 {
-    while(1){
-        robot.walk();
+    while(1) {
+        for(int i = 0; i < 4; i++) {
+            robot.walk();
+        }
+        for(int i = 0; i < 12; i++) {
+            robot.turn();
+        }
+
     }
     return 0;
 }
